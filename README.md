4e0d620100fe61781cd89df486510c3417724583
Paidn <63188636+Paidn@users.noreply.github.com>
2021-04-28 12:24:35 +0200

    Added automatic function

--- README.md
+++ README.md
@@ -2,13 +2,12 @@
 A small script to get around the annoying autostart behavior of Spotify!
 
 # Install
-To Install this script just clone this repo, run the installer script and follow the instructions.
+## Spotify Settings
+Please set your Spotify settings as shown (unser advanced settings):
+![Spotify Settings](spotify_settings.png)
 
+## Script Install
+To Install this script just clone this repo, run the installer script and follow the instructions.
 The script will be located at %appdata%\Microsoft\Windows\Start Menu\Programs\Paidn\spotify_closer.bat
 
-You can customize the waiting time for spotify to launch to meet your requirements.
-
-
-
-# ToDo
-Update the script that it automatically detects whether spotify is already opened or not
+You can customize the timeout for spotify to launch to meet your requirements.
--- bin/spotify_closer.bat
+++ bin/spotify_closer.bat
@@ -1,18 +1,26 @@
 @echo off
-
+title Spotify Closer
 :: ### This script is used to close spotify after startup ###
 :: # Please set your spotify settings to start up spotify minimized and close to tray #
 :: # This script will only kill the opened desktop window and not spotify running in tray #
 
-:: # Delay in seconds to wait before execution. Set this to match you startup precedure to make sure the window is open when this script runs #
+set timer=0 
+tasklist > nul
 
-set delay="2"
+:WAIT
+ping -n 1 -w 200 localhost > nul
+set /a  timer = %timer% + 1
+if %timer% GTR 50 (
+  exit
+) else (
+  tasklist /FI "Spotify.exe" 2>NUL | find /I /N "Spotify.exe">NUL | if "%ERRORLEVEL%"=="0" (
+    goto SPOKILL
+  ) else (
+    goto WAIT
+  )
+)
 
-
-:: ### Do not change anthing down below unless you know what your doing! ##
-
-ping -n %delay% localhost > nul
-
-taskkill /IM spotify.exe
-
+:SPOKILL
+ping -n 1 -w 200 localhost
+taskkill /IM Spotify.exe
 exit
